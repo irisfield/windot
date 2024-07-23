0df344066cf7fbc42ffb5e7e0299bb2112ae26e5
irisfield <aquezadawa@gmail.com>
2024-07-23 20:42:27 +0900

    added additional git configs

--- WindowsPowerShell/Microsoft.PowerShell_profile.ps1
+++ WindowsPowerShell/Microsoft.PowerShell_profile.ps1
@@ -8,6 +8,8 @@ if (!(Test-Path -Path $env:GIT_CONFIG_GLOBAL)) {
   New-Item -Path $env:GIT_CONFIG_GLOBAL -ItemType File -Force | Out-Null
   Invoke-Expression "git config --global core.editor nvim"
   Invoke-Expression "git config --global core.autocrlf false"
+  Invoke-Expression "git config --global credential.helper manager"
+  Invoke-Expression "git config --global init.defaultBranch master"
   Write-Host "Git config file location set to $($env:GIT_CONFIG_GLOBAL)." -ForegroundColor Blue
 }
 
