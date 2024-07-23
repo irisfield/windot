b102767164652229400e913ed21220f682d5555a
irisfield <aquezadawa@gmail.com>
2024-07-23 18:51:43 +0900

    firefox bookmarks and settings

---
+++ firefox/user.js
@@ -0,0 +1,11 @@
+user_pref("browser.startup.page", 3);
+user_pref("app.normandy.first_run", false);
+user_pref("browser.toolbars.bookmarks.visibility", "always");
+user_pref("privacy.donottrackheader.enabled", true);
+user_pref("privacy.fingerprintingProtection", true);
+user_pref("browser.contentblocking.category", "strict");
+user_pref("privacy.trackingprotection.enabled", true);
+user_pref("privacy.trackingprotection.emailtracking.enabled", true);
+user_pref("privacy.trackingprotection.socialtracking.enabled", true);
+user_pref("privacy.globalprivacycontrol.enabled", true);
+user_pref("privacy.globalprivacycontrol.was_ever_enabled", true);
