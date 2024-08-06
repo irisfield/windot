2cf1614319e6d471388e879eef29de3211e96b1b
irisfield <aquezadawa@gmail.com>
2024-08-06 15:03:07 +0900

    set up completion and which-key plugins

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -1,3 +1,121 @@
+local plugins = {
+  {
+    "rebelot/kanagawa.nvim",
+    lazy = false, -- load this during startup if it is your main colorscheme
+    priority = 1000, -- load this before all the other start plugins
+  },
+
+  {
+    "folke/which-key.nvim",
+    event = "VeryLazy",
+  },
+
+  {
+    "elihunter173/dirbuf.nvim",
+    cmd = {  "Dirbuf" }, -- lazy-load on command
+    opts = {
+      sort_order = "directories_first",
+      write_cmd = "DirbufSync -confirm",
+    }
+  },
+
+  {
+    "hrsh7th/nvim-cmp",
+    event = "InsertEnter", -- lazy-load on event
+    -- dependencies are always lazy-loaded unless specified otherwise
+    dependencies = {
+      "hrsh7th/cmp-nvim-lua",
+      "hrsh7th/cmp-buffer",
+      "FelipeLema/cmp-async-path",
+      "hrsh7th/cmp-nvim-lsp-signature-help",
+    },
+    config = function()
+      local cmp = require("cmp")
+      -- needed to ensure super tab works as intended
+      local check_backspace = function()
+        local col = vim.fn.col(".") - 1
+        return col == 0 or vim.fn.getline("."):sub(col, col):match("%s")
+      end
+      -- setup
+      cmp.setup({
+      mapping = {
+          ["<C-y>"] = cmp.config.disable,
+          ["<C-j>"] = cmp.mapping(cmp.mapping.scroll_docs(-1), { "i", "c" }),
+          ["<C-k>"] = cmp.mapping(cmp.mapping.scroll_docs(1), { "i", "c" }),
+          ["<C-e>"] = cmp.mapping({ i = cmp.mapping.abort(), c = cmp.mapping.close() }),
+          ["<CR>"] = cmp.mapping.confirm({ select = false }),
+          -- super tab
+          ["<Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_next_item()
+            elseif check_backspace() then
+              fallback()
+            else
+              fallback()
+            end
+          end, { "i", "s", }),
+          ["<S-Tab>"] = cmp.mapping(function(fallback)
+            if cmp.visible() then
+              cmp.select_prev_item()
+            else
+              fallback()
+            end
+          end, { "i", "s", }),
+        },
+        sources = {
+          { name = "nvim_lsp" },
+          { name = "buffer", keyboard_length = 2 },
+          { name = "async_path" },
+          { name = "nvim_lsp_signature_help" }
+        },
+        confirm_opts = {
+          behavior = cmp.ConfirmBehavior.Replace,
+          select = false,
+        },
+        experimental = {
+          ghost_text = true,
+          native_menu = false,
+        }
+      })
+    end,
+  },
+
+  -- if some code requires a module from an unloaded plugin, it will be automatically loaded.
+  -- So for api plugins like devicons, we can always set lazy=true
+  {
+    "nvim-tree/nvim-web-devicons",
+    lazy = true,
+  },
+
+  {
+    "iamcco/markdown-preview.nvim",
+    cmd = {  "MarkdownPreview" }, -- lazy-load on command
+    ft = { "markdown" }, -- lazy-load on filetype
+    build = function()
+      local download_url = "https://api.github.com/repos/iamcco/markdown-preview.nvim/releases"
+      local extract_path = vim.fn.stdpath("data") .. "\\lazy\\markdown-preview.nvim\\app\\bin"
+      local ps_script = [[
+        # Download zip file to download path
+        (Invoke-RestMethod "%s").assets | Where-Object { $_.name -like "*win.zip" } |
+        Select-Object -First 1 | ForEach-Object {
+          Invoke-WebRequest -Uri $_.browser_download_url -OutFile "${env:TEMP}\mdp-win.zip"
+        }
+
+        # Extract zip file to extract path and clean up
+        Expand-Archive -Path "${env:TEMP}\mdp-win.zip" -DestinationPath "%s" -Force
+        Remove-Item -Path "${env:TEMP}\mdp-win.zip" -Force
+      ]]
+      local ps_script = string.format(ps_script, download_url, extract_path)
+
+      print "Installing the required binary markdown-preview-win.exe..."
+      local pipe = io.popen("powershell -command -", "w")
+      pipe:write(ps_script)
+      pipe:close()
+      print("markdown-preview-win.exe was installed successfully!")
+    end,
+  },
+}
+
 -- Bootstrap lazy.nvim
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not (vim.uv or vim.loop).fs_stat(lazypath) then
@@ -16,87 +134,4 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 -- Setup lazy.nvim
-require("lazy").setup({
-  {
-    "rebelot/kanagawa.nvim",
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
-    priority = 1000, -- make sure to load this before all the other start plugins
-  },
-
-  -- I have a separate config.mappings file where I require which-key.
-  -- With lazy the plugin will be automatically loaded when it is required somewhere
-  { "folke/which-key.nvim", lazy = true },
-
-  {
-    "nvim-neorg/neorg",
-    -- lazy-load on filetype
-    ft = "norg",
-    -- options for neorg. This will automatically call `require("neorg").setup(opts)`
-    opts = {
-      load = {
-        ["core.defaults"] = {},
-      },
-    },
-  },
-
-  {
-    "hrsh7th/nvim-cmp",
-    -- load cmp on InsertEnter
-    event = "InsertEnter",
-    -- these dependencies will only be loaded when cmp loads
-    -- dependencies are always lazy-loaded unless specified otherwise
-    dependencies = {
-      "hrsh7th/cmp-nvim-lsp",
-      "hrsh7th/cmp-buffer",
-    },
-  },
-
-  -- if some code requires a module from an unloaded plugin, it will be automatically loaded.
-  -- So for api plugins like devicons, we can always set lazy=true
-  { "nvim-tree/nvim-web-devicons", lazy = true },
-
-  -- you can use the VeryLazy event for things that can
-  -- load later and are not important for the initial UI
-  { "stevearc/dressing.nvim", event = "VeryLazy" },
-
--- install without yarn or npm
-  {
-      "iamcco/markdown-preview.nvim",
-      cmd = {  "MarkdownPreview", "MarkdownPreviewStop", "MarkdownPreviewToggle" },
-      ft = { "markdown" },
-      build = function()
-        local download_url = "https://api.github.com/repos/iamcco/markdown-preview.nvim/releases"
-        local extract_path = vim.fn.stdpath("data") .. "\\lazy\\markdown-preview.nvim\\app\\bin"
-        local ps_script = [[
-          # Download zip file to download path
-          (Invoke-RestMethod "%s").assets | Where-Object { $_.name -like "*win.zip" } |
-          Select-Object -First 1 | ForEach-Object {
-            Invoke-WebRequest -Uri $_.browser_download_url -OutFile "${env:TEMP}\mdp-win.zip"
-          }
-
-          # Extract zip file to extract path
-          Expand-Archive -Path "${env:TEMP}\mdp-win.zip" -DestinationPath "%s" -Force
-
-          # Clean up
-          Remove-Item -Path "${env:TEMP}\mdp-win.zip" -Force
-        ]]
-        local ps_script = string.format(ps_script, download_url, extract_path)
-
-        print "Installing the required binary markdown-preview-win.exe..."
-        local pipe = io.popen("powershell -command -", "w")
-        pipe:write(ps_script)
-        pipe:close()
-        print "markdown-preview-win.exe was installed successfully!"
-      end,
-  },
-
-  -- local plugins can also be configured with the dev option.
-  -- This will use {config.dev.path}/noice.nvim/ instead of fetching it from GitHub
-  -- With the dev option, you can easily switch between the local and installed version of a plugin
-  { "folke/noice.nvim" },
-  -- Configure any other settings here. See the documentation for more details.
-  -- colorscheme that will be used when installing plugins.
-  install = { colorscheme = { "habamax" } },
-  -- automatically check for plugin updates
-  checker = { enabled = true },
-})
+require("lazy").setup(plugins)
